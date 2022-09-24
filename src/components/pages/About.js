f4e8cfa6f3554b9a80024d825ae432bf0af18558
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-09-24 18:13:14 -0400

    added about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+    return (
+      <div>
+        <h2>About Me</h2>
+        <p>
+            I am a full stack web developer with a background in biology, chemistry, and teaching. I have a passion for learning and I am always looking for
+            new ways to improve my skills. I recently completed the University of North Carolina's Full Stack Web Development Bootcamp and I am looking for
+            opportunities to apply my skills in a professional setting.
+        </p>
+        </div>
+    );
+}
+
